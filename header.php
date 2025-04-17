e7c064b433a0a2936bad666f026e0ff9ef2fe454
Vedantkumar Patel <96677832+cyberkiller76@users.noreply.github.com>
2025-04-17 15:17:06 -0400

    Add files via upload

---
+++ header.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Final Exam</title>
+    <link rel="icon" type="image/png" href="https://algomau.ca/wp-content/uploads/2020/02/favicon-32x32-1.png">
+
+</head>
+<body>
